ba2e6d36e508ad3bcb314c81b385686bff5b3ebf
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2024-06-27 18:08:37 -0300

    Update train_faster_rcnn.py

--- scripts/torch_models/train_faster_rcnn.py
+++ scripts/torch_models/train_faster_rcnn.py
@@ -14,17 +14,17 @@ from utils.clear_memory import clear_memory
 
 IN_DIR = '../../data'
 
-nusc = nuscenes.NuScenes(version='v1.0-mini', dataroot=IN_DIR, verbose=True)
+nusc = nuscenes.NuScenes(version='v1.0-trainval', dataroot=IN_DIR, verbose=True)
 
 BATCH_SIZE = 32 # increase / decrease according to GPU memory
 RESIZE_PERCENT = 1 # resize the image for training and transforms
-NUM_EPOCHS = 100 # number of epochs to train for
+NUM_EPOCHS = 10 # number of epochs to train for
 
 
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-TRAIN_NAME = 'mini_train'
-VALIDATION_NAME = 'mini_val'
+TRAIN_NAME = 'train'
+VALIDATION_NAME = 'val'
 # classes: 0 index is reserved for background
 CLASSES = [
     'background', *[category['name'] for category in nusc.category]
